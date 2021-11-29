ca462511d5d6898bb66975a608d49b7e0bb9824d
AbubakirovRA <AbubakirovRA@ya.ru>
2021-11-29 21:20:03 +0500

    add task 1-7 in branch lesson4

--- homeworks/lesson4/task6.py
+++ homeworks/lesson4/task6.py
@@ -13,3 +13,37 @@
 # Пример словаря:
 
 # {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
+my_dict = {}
+with open("text_6.txt", encoding="utf-8") as fobj:
+    for line in fobj:
+        name, stats = line.split(':')
+        name_sum = sum(map(int, "".join([i for i in stats if i == ' ' or '9' >= i >= '0']).split()))
+        print(f'{my_dict}')
+
+with open("text_6.txt", 'r', encoding="utf-8") as text_file:
+    a = text_file.readlines()
+    for s in a:
+        new_str = ''.join((i if i in '1234567890' else ' ') for i in s)
+        new_2 = [int(1) for i in new_str.split()]
+        print(f'{s.split()[0]} {sum(new_2)}')
+
+dic = {}
+numbers = '1234567890'
+with open("text_6.txt", 'r', encoding="utf-8") as file:
+    for line in file:
+        head, hours = line.split(":")
+        dic[head] = sum(map(int, ''.join([num for num in hours if num in numbers]).split()))
+print(dic)
+
+result = {}
+with open("text_6.txt", encoding="utf-8") as f_obj:
+    for line in f_obj:
+        lesson_timing = []
+        lessons = ([el for el in line.split("")])
+        for el in lessons:
+            lesson_timing.append(''.join(i for i in list(el) if i.isdigit()))
+        result[line.split(':')[0]] = sum([int(1) for i in lesson_timing if i.isdigit()])
+print(result)
+
+
+
--- homeworks/lesson4/task7.py
+++ homeworks/lesson4/task7.py
@@ -17,3 +17,9 @@
 
 # [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 # Подсказка: использовать менеджеры контекста.
+import json
+with open('my_ex7.json', 'w', encoding='utf-8') as write_f:
+    with open('text_7.txt', encoding='utf-8') as f_obj:
+        profit = {line.split()[0]: int(line.split()[2]) - int(line.split()[3]) for line in f_obj}
+        result = [profit, {'average_profit': round(sum([int(i) for i in profit.values() if int(i) > 0]) / len([int(i) for i in profit.values() if int(i) > 0]))}]
+    json.dump(result, write_f, ensure_ascii=False, indent=4)
